44236488ac46120e89540394cc7a53edb4a08b13
itay <itay289@gmail.com>
2014-10-01 17:49:53 +0300

    fixing stupid shit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,43 +1,11 @@
 class TopicsController < ApplicationController
 
 	def show
-		@sub_topics = Sub_topic.find(params[:id])
+
 	end
 
 	def index
-		@sub_topics = Sub_topic.all
+		@topics = Topic.all		
 	end
-
-	def new
-		@sub_topics = Sub_topic.new
-	end
-
-	def create
-		@sub_topics = Sub_topic.new(params[:id])
-		if@Sub_topic.save
-			flash[:success] = "Subject created successfully"
-    else
-    	render 'new'	
-    end	
-	end
-
-	def destroy
-		@sub_topics = Sub_topic.find(params[:id]).destroy
-		flash[:success] = "Subject destroyed."
-	end
-
-	private
-
-    def signed_in_user
-      unless signed_in?
-        store_location # set at the session helper
-        redirect_to signin_path, notice: "Please sign in." unless signed_in?
-      end  
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to root_path unless current_user?(@user)
-    end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'home#index'
-  
-  resources :home
+  root to: 'topics#index'
+
+  resources :subtopics
 
   resources :topics do
-  	resources :comments
+  	
   end	
 
   resources :sessions
