a4ea3d1362efeae75cdebe558fd1833ba9cc3ecb
shahaf <shahaf255@gmail.com>
2014-10-29 12:09:44 +0200

    minor fix

--- config/god/main.rb
+++ config/god/main.rb
@@ -6,7 +6,7 @@ God.watch do |w|
   pid_file = "#{God.pid_file_directory}/unicorn.pid"
   
   w.name = "unicorn"
-  w.start = "bundle exec unicorn -c #{config_path}/unicorn.rb -E #{ENV["RAILS_ENV"]} -D"
+  w.start = "bundle exec unicorn -c #{config_path}/unicorn.rb -E #{ENV["RAILS_ENV"]}"
   w.restart = "kill -s USR2 $(cat #{pid_file})"
   w.keepalive(memory_max: 150.megabytes, cpu_max: 50.percent)
 
