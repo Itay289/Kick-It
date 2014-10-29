b6e19f7c570d841135ee8cae863f5e54c9d61809
shahaf <shahaf255@gmail.com>
2014-10-29 14:53:07 +0200

    god changes

--- config/god/main.rb
+++ config/god/main.rb
@@ -1,4 +1,5 @@
 config_path = File.expand_path(File.join(File.dirname(__FILE__), "../"))
+rails_root = "/home/deploy/Kick-It/current"
 
 God.pid_file_directory = '/home/deploy/Kick-It/shared/pids'
 
@@ -6,7 +7,7 @@ God.watch do |w|
   pid_file = "#{God.pid_file_directory}/unicorn.pid"
   
   w.name = "unicorn"
-  w.start = "bundle exec unicorn -c #{config_path}/unicorn.rb -E #{ENV["RAILS_ENV"]}"
+  w.start = "cd #{rails_root} && BUNDLE_GEMFILE=#{rails_root}/Gemfile bundle exec unicorn -c #{config_path}/unicorn.rb -E #{ENV["RAILS_ENV"]} -D"
   w.restart = "kill -s USR2 $(cat #{pid_file})"
   w.keepalive(memory_max: 150.megabytes, cpu_max: 50.percent)
 
