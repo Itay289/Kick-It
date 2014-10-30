9e22b2e298edaa00cc2f2ad2fb6532a9fcc1a91e
Erez Rabih <erez.rabih@gmail.com>
2014-10-30 10:24:44 +0200

    moved show_name from application controller to sub_topics helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  helper_method :show_name
-
-  def show_name(subject)
-    subject.created_by.to_s.split('@').first
-  end
 end
