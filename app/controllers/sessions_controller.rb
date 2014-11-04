a3912a3309d05374bde9f1e1571dcd8db94595e3
shahaf <shahaf255@gmail.com>
2014-11-04 12:03:00 +0200

    minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,26 @@
 class SessionsController < ApplicationController
 
-	def create
-		@user_data = User.new(mail: params[:session][:mail])
-		user = User.find_by(mail: params[:session][:mail])
-		if @user_data.valid?
-			if user  
-				flash[:success] = "Hi #{user.mail.to_s.split('@').first} welcome back"   	
-			else 
-				user = User.create mail: params[:session][:mail]
-				flash[:success] = "Welcome #{user.mail.to_s.split('@').first}"   
-			end	
-			sign_in user
-			redirect_back_to root_path
-		else
-			flash[:error] = "Please fill in valid information"
+  def create
+    @user_data = User.new(mail: params[:session][:mail])
+    user = User.find_by(mail: params[:session][:mail])
+    if @user_data.valid?
+      if user  
+        flash[:success] = "Hi #{user.mail.to_s.split('@').first} welcome back"    
+      else 
+        user = User.create mail: params[:session][:mail]
+        flash[:success] = "Welcome #{user.mail.to_s.split('@').first}"   
+      end 
+      sign_in user
+      redirect_to root_path
+    else
+      flash[:error] = "Please fill in valid information"
       redirect_to :back 
-    end  	      	
-	end
+    end           
+  end
 
-	def destroy
-		sign_out
-		redirect_to :back
-	end
+  def destroy
+    sign_out
+    redirect_to :back
+  end
 
 end
